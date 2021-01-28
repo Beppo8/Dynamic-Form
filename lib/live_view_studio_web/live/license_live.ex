3aa18f2f0af64ecad43e4a1e60abce8db7829994
beppo <beppo@localhost.localdomain>
2021-01-28 07:42:52 -0600

    setting up render()

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -2,15 +2,40 @@ defmodule LiveViewStudioWeb.LicenseLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Licenses
+  import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(scoket, seats: 2, amount: Licenses.calculate(2))
+    socket = assign(socket, seats: 2, amount: Licenses.calculate(2))
     {:ok, socket}
   end
 
   def render(assigns) do
     ~L"""
+      <h1>Team License</h1>
+      <div id="license">
+        <div class="card">
+          <div class="content">
+            <div class="seats">
+              <img src="images/license.svg">
+              <span>
+                Your license is currently for
+                <strong><%= @seats %></strong> seats.
+              </span>
+            </div>
 
+            <form>
+              <input type="range" min="1" max="10"
+                    name="seats" value="<%= @seats %>" />
+            </form>
+
+            <div class="amount">
+              <%= number_to_currency(@amount) %>
+            </div>
+
+          </div>
+        </div>
+      </div>
     """
   end
+
 end
